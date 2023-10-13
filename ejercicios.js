716d0c9c15e8836b5338b00e3090b5d92d763164
MKGBS <fullstackvlc@gmail.com>
2023-10-13 14:39:15 +0200

    ejercicio de bucles

---
+++ ejercicios.js
@@ -0,0 +1,145 @@
+//1 Imprimir los números del 21 al 34 en la consola.
+
+// let i = 5;
+// for (let i = 21; i < 35; i++) {
+//     console.log(i);
+//   }
+
+//2 Imprimir los números del 1 al 10 en la consola, pero solo los pares.
+// const numbers = [1,2,3,4,5,6,7,8,9,10]; 
+//   for (let i = 1; i < 10; i++)
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// 3 Imprimir los números del 1 al 10 en la consola, pero solo los impares. 
+// const number = [1,2,3,4,5,6,7,8,9,10];
+// for (let i = 0; i < number.length; i++) {
+//   if (i % 2 != 0) {
+//     console.log(i);
+//   }
+
+// } 
+
+//
+//4 Dado un array de números, imprimir en la consola solo los números pares.
+
+// const numeros = [1,2,3,4,5,6,7,8,9,10]; 
+//   for (let numeros = 1; numeros < 10; numeros++)
+//   if (numeros % 2 === 0) {
+//     console.log(numeros);
+//   }
+
+//5 Dado un array de números, imprimir en la consola solo los números mayores a 5.
+// const numerosMayor5 = [1,2,3,4,5,6,7,8,9,10];
+// for (let i = 0; i < numerosMayor5.length; i++) {
+//   if (i >= 5) {
+//     console.log(i)
+//   }
+// }
+
+
+//6 Dada un array de nombres, imprimir solo los nombres que empiezan con la letra "A".
+
+// let namelist =[
+//   {nombre:'Ramiro'},
+//   {nombre:'Marc'},
+//   {nombre:'Jairo'},
+//   {nombre:'Angel'},
+//   {nombre:'Anatory'},
+//   {nombre:'Isabel'},
+//   {nombre:'Marta'},
+//   {nombre:'Jaime'},
+//   {nombre:'Augusto'}
+// ]
+
+// const nameA = []
+// for (let i = 0; i < namelist.length; i++) {
+//   //console.log(namelist[i].nombre[0])
+//    if (namelist[i].nombre [0] == "A") {
+//       //console.log(namelist[i]);
+//       nameA.push(namelist[i]);
+//   }
+// }
+// console.log(nameA)
+
+
+//7 Dada un array de números, encontrar el número más grande y el número más pequeño.
+
+// const numeros = [2,3,5,6,7,9,11,15];
+// let minimo = numeros[0] 
+// let maximo = numeros[0]  
+
+// for (const numero of numeros) {
+//   if (numero > maximo) {
+//     maximo = numero
+// }
+// if (numero < minimo) {
+//   minimo = numero
+// }
+// }
+// console.log("El número más grande es:", maximo)
+// console.log("El número más pequeño es:", minimo)
+
+// const numeros = [2, 5, 10, 1, 8, 3, 6]
+
+// let maximo = numeros[0]
+// let minimo = numeros[0]
+
+// for (const numero of numeros) {
+//     if (numero > maximo){
+//         maximo = numero
+//     }
+//     if (numero < minimo){
+//         minimo = numero
+//     }
+// }
+// console.log("El número más grande es:", maximo)
+// console.log("El número más pequeño es:", minimo)
+
+//8 Haz un bucle que muestre por consola los números del 4 al 9 inclusive. Utilizad el bucle for.
+
+//  const nume = [1,2,3,4,5,6,7,8,9,10];
+//  for (let i = 4; i <10; i++) {
+//   console.log(i)
+//  }
+
+//9 Haz un bucle que muestre por consola los impares del 3 al 17 inclusive. Utilizad el bucle for.
+
+// const number = [1,2,3,4,5,6,7,8,9,10,12,13,17];
+// for (let i = 3; i <18; i++) {
+//   if (i % 2 != 0) {
+//     console.log(i);
+//   }
+// } 
+
+//10 Crear variable contarHasta10 con valor 0 e incrementar su valor con un bucle hasta que se verifique que contarHasta10 === 10
+// const sumaPares = [1,2,3,4,5,6,7,8,9,10,12,13,17];
+
+// let contarHasta10 = 0, res = 10;
+// while (contarHasta10 < 10) {
+//     if (contarHasta10 % 2 === 10)
+//         res += i; i++;
+// }
+// confirm.console.log(sumaPares).toBe(res);
+
+//11 Crea una variable arrayBucle tipo array vacía, rellénala con números del 4 al 18 utilizando un bucle
+ArrayBucle = []
+
+for (let i = 0; i < 19; i++) {
+    
+}
+
+
+//12 Recorre la variable creada anteriormente arrayBucle ,suma todos sus elementos y guárdalos en una variable de nombre resultado
+
+//13 Dado el siguiente array, [‘Con’, ‘Sofia’, ‘aprendiendo’, ‘bucles’], recorrelo utilizando el bucle “for of” y muestra por consola todos sus elementos, después recórrelo utilizando el bucle “for in” mostrando también por consola todos sus elementos
+
+//14 Crea un bucle utilizando “while” que itere 20 veces y muestre por consola “Patata” cada vez que el número de la iteración sea múltiplo de 3
+
+//15 Sumar los números del 1 al 10 y mostrar el resultado en la consola.
+
+//16 Dado un array de números, imprimir en la consola la suma de todos los números.
+
+//17 Imprimir los números del 1 al 20 en la consola, pero para los múltiplos de 3 imprimir "Fizz", para los múltiplos de 5 imprimir "Buzz" y para los múltiplos de ambos imprimir "FizzBuzz".
+
+
